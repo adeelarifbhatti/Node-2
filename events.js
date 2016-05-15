c5cab4675b30684209f879893413dc64ec677b7a
adeelarifbhatti <adeelarifbhatti@gmail.com>
2016-05-15 23:24:32 +0200

    Events would be generated partially and the program will be broken when the error is thrown. This is the eventemitter approach which shows events are generated until error is  occured. I will implement same kind of approach in callback and check if the events are generated partially or not.

--- events.js
+++ events.js
@@ -1,17 +1,32 @@
 var EventEmitter = require('events').EventEmitter;
 
-var getEvent = function() {
+var getEvent = function(v) {
  var e = new EventEmitter();
  setTimeout(function() {
-  e.emit('triggered');
-  console.log("from function");
-  },900);
+  for (var i=1;i<v;i++) {
+   e.emit('triggered',i);
+   if (i===7) {
+    console.log("this is from new Error");
+    return new Error( "this is error"); 
+   } 
+   if (i===4)
+   e.emit('db-connection', i);
+   if (i===6)
+   e.emit('connection-dropped',i);
+  }
+ },900);
  return(e);
 };
 
-var r = getEvent();
-
-r.on('triggered', function() {
- console.log("this is start");
+var r = getEvent(13);
+r.on('triggered', function(i) {
+ console.log("start event sent at " + i);
  });
 
+r.on('db-connection', function(d) {
+ console.log("we got data " + d); 
+ });
+
+r.on('connection-dropped', function(v) {
+ console.log("this is end " + v);
+ });
