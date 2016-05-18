68f996847dc9f65a34fcbc466f891514fb01ec1d
adeelarifbhatti <adeelarifbhatti@gmail.com>
2016-05-18 10:51:21 +0200

    I was hoping that this would not run properly because of the error which I threw, but it manages to work. Actually what I learnt was it would print unless all the results are processed. But prictically things have gone in different way.

--- callback.js
+++ callback.js
@@ -1,15 +1,23 @@
 var addingNumber = function(v, callback) {
+ for (var i=0;i<10;i++){
+  if (v<7) { 
   callback(null,v+v);
+  v++;
   }
+ else callback(new Error("v is greater than 7"));
+ }
+}
  
 var results = function(err, plus) {
  if (err) {
- 	console.log("what went wrong is"+ err.message);
- } else {
-	console.log("results are: " + plus);
-   }
-};
-addingNumber(10,results);
+  console.log("what went wrong is "+ err.message);
+  }
+ else {
+  console.log("results are: " + plus);
+ }
+}
+
+addingNumber(2,results);
 
  
   
