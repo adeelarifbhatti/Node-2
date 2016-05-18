e392df91bf97aabf2f7ba72c8d36ae44eec7e1b9
adeelarifbhatti <adeelarifbhatti@gmail.com>
2016-05-18 14:30:35 +0200

    trying to code in a way that the event is emitted based on user's input

--- events.js
+++ events.js
@@ -1,32 +1,31 @@
 var EventEmitter = require('events').EventEmitter;
 
-var getEvent = function(v) {
+var getEvent = function(s) {
+ if (isNaN(s)); {
  var e = new EventEmitter();
  setTimeout(function() {
-  for (var i=1;i<v;i++) {
-   e.emit('triggered',i);
-   if (i===7) {
-    console.log("this is from new Error");
-    return new Error( "this is error"); 
-   } 
-   if (i===4)
-   e.emit('db-connection', i);
-   if (i===6)
-   e.emit('connection-dropped',i);
+  for (var i=1;i<10;i++) {
+console.log(s,i);
+   e.emit(s,i);
   }
- },900);
+ },200);
  return(e);
+}
 };
-
-var r = getEvent(13);
-r.on('triggered', function(i) {
- console.log("start event sent at " + i);
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdout.write('choose one out of these three \n 1-database \n 2-connection string \n 3-connection dropped \n');
+process.stdin.on('data', function(c) { 
+var r = getEvent(c);
+r.on('database', function(i) {
+ console.log("start event sent at " + i + ". User entered "+ c);
  });
 
-r.on('db-connection', function(d) {
+r.on('connection string', function(d) {
  console.log("we got data " + d); 
  });
 
-r.on('connection-dropped', function(v) {
+r.on('connection dropped', function(v) {
  console.log("this is end " + v);
+});
  });
