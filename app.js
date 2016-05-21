96981e2f7ab0e4b30f426404e90df45f312d84dc
root <adeelarifbhatti@gmail.com>
2016-05-21 06:47:08 +0200

    created a new file app.js, it has imported express. using app.use(express.static('public')) for the public/static files. I don't want to create rules for each folder like app.get('/', function(req,res){...
    So express check the request in static folders (public and srv/view) first, if it doesn't find
    stuff then it will look down for the rules.

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+var port = 8080;
+
+app.use(express.static('public'));
+
+app.use(express.static('src/view'));
+ 
+app.get('/', function(req,res){
+	res.send('very basic first app, kind of hello world');
+});
+
+app.listen(port, function(err) {
+	console.log("the server on port 8080");
+
+
+});
