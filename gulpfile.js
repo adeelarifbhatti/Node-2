9c9355870f0c5c70e637f51000bec04aa338efae
root <adeelarifbhatti@gmail.com>
2016-05-23 11:23:23 +0200

    Modified the gulpfile.js to add a gulp task update for updating the scripts files for css,js, tether, awesome-fonts. As I have installed the dependencies using bower, so bower.json file contains the information about what dependencies's scripts to add in the html.
    It checking the directory /scr/lib for bootstrap and futher looks into the bower.json to check dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
  var gulp = require('gulp');
  var jshint = require('gulp-jshint');
+ var wiredep = require('wiredep').stream;
  //var jscs = require('gulp-jscs');
 
- var checkFiles = ['*.js','/src/lib/*.js'];
+ var checkJs = ['*.js','/src/lib/*.js'];
  gulp.task('style', function() {
- 	gulp.src(checkFiles)
+ 	gulp.src(checkJs)
  	.pipe(jshint())
  	.pipe(jshint.reporter('jshint-stylish', {
  		verbose: true
@@ -12,3 +13,16 @@
  	//.pipe(jscs());
 
   });
+
+ gulp.task('update', function() {
+ 	var checkHtml = './src/view/*.html';
+ 	var destHtml = './src/view';
+ 	var bowerOptions = {
+ 		bowerJson: require('./bower.json'),
+ 		directory: './public/lib'
+ 	}
+ 	return gulp.src(checkHtml)
+ 	.pipe(wiredep(bowerOptions))
+ 	.pipe(gulp.dest(destHtml));
+
+ });
