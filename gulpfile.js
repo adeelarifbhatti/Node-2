05cafa50ae25e4b9a8aaebbbe6a33d1987c1c0d9
root <adeelarifbhatti@gmail.com>
2016-05-23 14:11:05 +0200

    I installed the gulp-inject in order to get our css/js files in the index.html via gulp. The latest verion had some dependencies issue, so I edited package.json to include a old version of gulp-inject i.e. 1.5.0. It worked fine. follwoing code was written in the gulpfile.js
    var inject = require('gulp-inject');
     var ourCssJs = gulp.src(['./public/css/*.css', './public/js/*.js'], {read: false});
     var ourOptions = { ignorePath: '/public'}
    ......pipe(inject(ourCssJs,ourOptions))
    
    "gulp update" added our css and js in the comments i.e.
    
      <!--inject:css-->
    
      <!--endinject-->
      <!--inject:js-->
    
      <!--endinject-->
    These comments have to be included in the index file ourself. Then I removed the css entry in the index.html which was shipped in by default.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
  var gulp = require('gulp');
  var jshint = require('gulp-jshint');
  var wiredep = require('wiredep').stream;
+ var inject = require('gulp-inject');
  //var jscs = require('gulp-jscs');
 
  var checkJs = ['*.js','/src/lib/*.js'];
@@ -22,8 +23,11 @@
  		directory: './public/lib',
  		ignorePath: '../../public'
  	}
+ 	var ourCssJs = gulp.src(['./public/css/*.css', './public/js/*.js'], {read: false});
+ 	var ourOptions = { ignorePath: '/public'}
  	return gulp.src(checkHtml)
  	.pipe(wiredep(bowerOptions))
+ 	.pipe(inject(ourCssJs,ourOptions))
  	.pipe(gulp.dest(destHtml));
 
  });
