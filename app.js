7aebac8e551600329017b10538a825766bbf728b
root <adeelarifbhatti@gmail.com>
2016-05-27 13:49:39 +0200

    I added all the pages from second to fifth and modified the app.js accordingly.

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 var firstRouter= express.Router();
-
+var secondRouter= express.Router();
+var thirdRouter= express.Router();
+var fourthRouter= express.Router();
+var fifthRouter= express.Router();
 
 app.use(express.static('public'));
 
@@ -16,6 +19,15 @@ The following method is for the index-backup page, it along with index-back has 
 app.get('/index-backup', function(req,res){
 	res.render('index-backup', {title: 'No Use of this Page', menu: ['First','Second','Third','Fourth','fifth','sixth','seventh']});
 });
+app.use('/first', firstRouter);
+app.use('/second', secondRouter);
+app.use('/third', thirdRouter);
+app.use('/fourth', fourthRouter);
+app.use('/fifth', fifthRouter);
+
+
+
+
 
 
 app.get('/', function(req,res){
@@ -42,7 +54,56 @@ firstRouter.route('/')
 																	
 				]});
 });
+	secondRouter.route('/')
+	.get(function (req,res) {
+		res.render('first', {title: 'From Second', sideMenu: [
 
+																	{Link: '/First', Text: 'First'}, 
+																	{Link: '/Second', Text: 'Second'}, 
+																	{Link: '/Third', Text: 'Third'}, 
+																	{Link: '/Fourth', Text: 'Fourth'}, 
+																	{Link: '/Fifth', Text: 'Fifth'}
+																	
+				]});
+});
+	thirdRouter.route('/')
+	.get(function (req,res) {
+		res.render('first', {title: 'From third', sideMenu: [
+
+																	{Link: '/First', Text: 'First'}, 
+																	{Link: '/Second', Text: 'Second'}, 
+																	{Link: '/Third', Text: 'Third'}, 
+																	{Link: '/Fourth', Text: 'Fourth'}, 
+																	{Link: '/Fifth', Text: 'Fifth'}
+																	
+				]});
+});
+
+fourthRouter.route('/')
+	.get(function (req,res) {
+		res.render('first', {title: 'From Fourth', sideMenu: [
+
+																	{Link: '/First', Text: 'First'}, 
+																	{Link: '/Second', Text: 'Second'}, 
+																	{Link: '/Third', Text: 'Third'}, 
+																	{Link: '/Fourth', Text: 'Fourth'}, 
+																	{Link: '/Fifth', Text: 'Fifth'}
+																	
+				]});
+});
+
+	fifthRouter.route('/')
+	.get(function (req,res) {
+		res.render('first', {title: 'From Fifth', sideMenu: [
+
+																	{Link: '/First', Text: 'First'}, 
+																	{Link: '/Second', Text: 'Second'}, 
+																	{Link: '/Third', Text: 'Third'}, 
+																	{Link: '/Fourth', Text: 'Fourth'}, 
+																	{Link: '/Fifth', Text: 'Fifth'}
+																	
+				]});
+});
 app.use('/first', firstRouter);
 
 
