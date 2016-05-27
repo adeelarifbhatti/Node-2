79e9a16cad938beff929bfbc4977c3e9b6319cc4
root <adeelarifbhatti@gmail.com>
2016-05-27 13:18:19 +0200

    I added a link first to index.ejs page, simply copied the index.ejs to first.ejs. Then I created the firstRouter in the app.js file using
     app.use('/first', firstRouter);
    
    and then defined the router i.e. what and where should it go.
    
    firstRouter.route('/')
    	.get(function (req,res) {
    		res.render('first', {title: 'my first app in EJS', sideMenu: [
    
    		{Link: '/First', Text: 'First'},
    		{Link: '/Second', Text: 'Second'},
    		{Link: '/Third', Text: 'Third'},
    		{Link: '/Fourth', Text: 'Fourth'},
    		{Link: '/Fifth', Text: 'Fifth'}
    
    		]});
    	});
    
    so it will go to /first > / i.e. /first/
    
    As I have deleted the ccs file for so it got eliminated via gulp thats why git reported changed in the index-backup file.

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ var express = require('express');
 
 var app = express();
 var port = process.env.PORT || 8080;
+var firstRouter= express.Router();
+
 
 app.use(express.static('public'));
 
 app.set('views','./src/view');
 app.set('view engine', 'ejs');
 
+/*
+The following method is for the index-backup page, it along with index-back has no use.
+*/
 app.get('/index-backup', function(req,res){
-	res.render('index-backup', {title: 'my first app in EJS', menu: ['First','Second','Third','Fourth','fifth','sixth','seventh']});
+	res.render('index-backup', {title: 'No Use of this Page', menu: ['First','Second','Third','Fourth','fifth','sixth','seventh']});
 });
+
 
 app.get('/', function(req,res){
 	res.render('index', {title: 'my first app in EJS', sideMenu: [
@@ -23,6 +29,22 @@ app.get('/', function(req,res){
 																	
 				]});
 });
+
+firstRouter.route('/')
+	.get(function (req,res) {
+		res.render('first', {title: 'my first app in EJS', sideMenu: [
+
+																	{Link: '/First', Text: 'First'}, 
+																	{Link: '/Second', Text: 'Second'}, 
+																	{Link: '/Third', Text: 'Third'}, 
+																	{Link: '/Fourth', Text: 'Fourth'}, 
+																	{Link: '/Fifth', Text: 'Fifth'}
+																	
+				]});
+});
+
+app.use('/first', firstRouter);
+
 
 app.listen(port, function(err) {
 	console.log("the server on port " + port);
