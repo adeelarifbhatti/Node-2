fcf998c58118d02305ec5998fa2b73c9d1152b32
root <adeelarifbhatti@gmail.com>
2016-05-30 22:26:05 +0200

    I added the values for some fake parameters in the firstroute.js file and called them in the first.ejs file via for loop ! var fakeData = [ { value1: 'fake value1', value2 : 'Microsoft', value3: 'fake value1' }, { value1: 'fake value2', value2 : 'Oracle', value3: 'fake value2' },
    {Link: '/Fifth', Text: 'Fifth'}
    
      ], fakeData: fakeData
       }
    
    <% for(var i=0; i<fakeData.length;i++) {%>
       <li>
       <a href="<%=fakeData[i].value3%>"> <h4><%=fakeData[i].value2%> </h4></a>
       </li>
       <%}%>

--- src/routes/firstRoute.js
+++ src/routes/firstRoute.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var firstRoute = express.Router();
 
+var fakeData = [
+				{
+					value1: 'fake value1', value2 : 'Microsoft', value3: 'fake value1'
+				},
+				{
+					value1: 'fake value2', value2 : 'Oracle', value3: 'fake value2'
+				},
+				{
+					value1: 'fake value3', value2 : 'IBM', value3: 'fake value3'
+				},
+				{
+					value1: 'fake value4', value2 : 'Dell', value3: 'fake value4'
+				},
+				{
+					value1: 'fake value5', value2 : 'Apple', value3: 'fake value5'
+				}
+			   ];
 
 
 
@@ -11,7 +28,8 @@ firstRoute.route('/').get(function (req,res){
 										{Link: '/Fourth', Text: 'Fourth'}, 
 										{Link: '/Fifth', Text: 'Fifth'}
 																
-				        ]}
+				        ], fakeData: fakeData
+				    }
 				);
 
  });
