b6d147fa3bddcc0390b13b185bcbf901cc31cae2
Adeel Arif <adeelarifbhatti@gmail.com>
2016-06-24 09:56:32 +0200

    The user check is been added to local.strategy.js, it checks if the user exists in the mongodb (users's collection).

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,17 +1,24 @@
 var passport = require('passport');
 var LocalStrategies = require('passport-local').Strategy;
+var mongodb = require('mongodb').MongoClient
 
 var localStrategyAuth = function () {
 	var userpass = new LocalStrategies({
 		usernameField: 'UserName',
 		passwordField: 'Password'
 	},function(username, password, done){
+		var url = 'mongodb://127.0.0.1:27017/db-express';
+		mongodb.connect(url, function(err, db) {
+		var collection = db.collection('users');
+		collection.findOne({username: username}, 
+			function(err, results){
+				var user = results;
+				done(null, user);
+			}
+		);
 
-		var user = {
-			username: username,
-			password: password
-		};
-		done(null,user);
+		
+		});
 	});
 
 	passport.use(userpass);
