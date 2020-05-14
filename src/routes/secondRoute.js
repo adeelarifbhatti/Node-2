a13cd935d28dfe9fa135c48ca7efcbe472aac54b
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2020-05-14 14:12:27 +0200

    Added the capital page with the names of capital which on click takes you to name of the country the capital belongs to

--- src/routes/secondRoute.js
+++ src/routes/secondRoute.js
@@ -1,13 +1,37 @@
 var express = require('express');
 var secondRoute = express.Router();
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID
 
 var router = function(sideMenu){
-secondRoute.route('/').get(function (req,res){
-res.render('second',{title: 'From Second', sideMenu: sideMenu });
-
-
+ secondRoute.route('/').get(function (req,res){
+	var url = 'mongodb://my-mongo:27017/db-express';
+	mongodb.connect(url, function(err,db){
+	var collection = db.collection('data')
+	collection.find({}).toArray(
+		function(err,results){
+			res.render('second',{title: 'From Second', 
+			sideMenu: sideMenu,fakeData: results });
+		}
+	);
+ });
 });
-return secondRoute;
-};
+ secondRoute.route('/:id').get(function(req,res){
+ 	var id = new objectId(req.params.id);
+ 	var url = 'mongodb://my-mongo:27017/db-express';
+ 		mongodb.connect(url, function(err, db){
+ 		var collection =  db.collection('data');
+ 		collection.findOne({_id :id},function(err,results){
+ 		
+ 			res.render('secondView', {title: 'From Second',
+		 	sideMenu: sideMenu,
+		 	fakeData: results});
+		});
+ 	});
+});
 
+
+return secondRoute;
+
+}
 module.exports = router;
