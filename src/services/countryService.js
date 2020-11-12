550820f77850c0498899aad72746cef718fa9686
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2020-11-12 01:47:31 +0100

    fetching the data from the api is partially working

--- src/services/countryService.js
+++ src/services/countryService.js
@@ -1,6 +1,29 @@
+var http = require('http');
 var countryService = function(){
 	var getCountryById = function(id,cb){
-		cb(null,{description: "My Country From Json"});
+		var options= {
+
+			host:'restcountries.eu',
+			path:'/rest/v2/name/pakistan'
+		};
+		var callback = function(response){
+			var api = '';
+			response.on('data', function(chunk){
+				api += chunk;
+			});
+			response.on('end', function() {
+                        
+            JSON.parse(api, function(err, result){
+            		cb(err,result);
+            		//cb(null,{description: "My Country From Json"});
+            		
+            });
+        });
+
+			};
+			http.request(options,callback).end();
+		
+		
 	};
 	
 	return {
