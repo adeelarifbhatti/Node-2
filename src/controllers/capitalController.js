8a056c3e03b22f3330804dfb13c61f58b1e6dc82
Adeel Arif Bhatti <adeel.bhatti@huddly.com>
2024-02-13 08:14:12 +0100

    Replaced the mondbclient/ObjectID with mongoose (Model/schema) for one capital/country(req.param.id).

--- src/controllers/capitalController.js
+++ src/controllers/capitalController.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var firstRoute = express.Router();
-var objectId = require('mongodb').ObjectID
 const cc = require('../model/ccModel');
 
 
@@ -8,16 +7,6 @@ var capitalController = function (sideMenu){
 
 	var getIndex = function (req,res){
  	if(req.user){
-// 	var url = 'mongodb://my-mongo:27017/db-express';
-// 	mongodb.connect(url, function(err,client){
-// 	var collection = client.db('db-express')
-// 	collection.collection('data').find({}).toArray(
-// 		function(err,results){
-// 			res.render('capitals',{title: 'From Second', 
-// 			sideMenu: sideMenu,fakeData: results });
-// 		}
-// 	);
-//  });
 	cc.find({}).then((results) => {
 	console.log(results);
 	res.render('capitals', {title: 'From First', sideMenu: sideMenu, fakeData: results});
@@ -25,17 +14,10 @@ var capitalController = function (sideMenu){
 }
 };
 	var getById = function(req,res){
- 	var id = new objectId(req.params.id);
- 	var url = 'mongodb://my-mongo:27017/db-express';
- 		mongodb.connect(url, function(err, client){
- 		var collection =  client.db('db-express');
- 		collection.collection('data').findOne({_id :id},function(err,results){
- 		
- 			res.render('countryview', {title: 'From Second',
-		 	sideMenu: sideMenu,
-		 	fakeData: results});
+	 cc.findById({_id :req.params.id}).then((results) => {
+		console.log(results);
+		res.render('countryview', {title: 'From First', sideMenu: sideMenu, fakeData: results});
 		});
- 	});
 };
 	var middleWare = function(req,res,next){
 		if(!req.user){
