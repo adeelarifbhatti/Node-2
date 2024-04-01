3c0198ed21be96d938d21e419dbce199d3093c76
Adeel Arif Bhatti <adeel.bhatti@huddly.com>
2024-04-01 14:45:45 +0200

    User login: Now the password check works
    I changed the promise to async/await

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -6,16 +6,26 @@ var localStrategyAuth = function () {
 	var userpass = new LocalStrategies({
 		usernameField: 'username',
 		passwordField: 'password'
-	},function(username, password, done) {
-		User.findOne({ username: username }).then(user => {
-		  if (!user) {
-			return done(null, false, { message: 'Incorrect username.' });
-		  }
-		  if (!user.checkPassword(password, user.password)) {
+	},async function(username, password, done) {
+		const user = await User.findOne({username}).select("+password");
+		if(!user){
+			return next(new ErrorHandler("Invalid Email or Password" ));
+		}	
+		const isPasswordMatched = await user.checkPassword(password, user.password);	
+		  if (!isPasswordMatched) {
 			return done(null, false, { message: 'Incorrect password.' });
 		  }
-		  return done(null, user);
-		});
+		return done(null, user);
+		// Old Code !!!
+		// User.findOne({ username: username }).then(user => {
+		//   if (!user) {
+		// 	return done(null, false, { message: 'Incorrect username.' });
+		//   }
+		//   if (!user.checkPassword(password, user.password)) {
+		// 	return done(null, false, { message: 'Incorrect password.' });
+		//   }
+		//   return done(null, user);
+		// });
 	  });
 
 	passport.use(userpass);
